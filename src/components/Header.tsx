56da38a511bf45358f64c3f1a679faee1b1df473
matthewsalas19 <matthew.urena19@gmail.com>
2026-08-04 18:17:45 -0500

    feat: verificacion de ticket

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-cafe-oscuro text-dorado px-6 py-6 shadow">
+      <div className="mx-auto max-w-6xl">
+        <p className="text-sm uppercase tracking-[0.3em] elegant">Sistema de Biblioteca</p>
+      </div>
+    </header>
+  )
+}
